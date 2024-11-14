83a0e61722ddfe8888fdc1a84c534c6fd2e6cf83
Ayush Pathak <ayushpathak@Masterusers-MacBook-Pro.local>
2024-11-14 21:21:37 +0530

    Added env variable in github

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import subprocess
+import os
 
 # Variables for the database connection
-DB_HOST = "jira-redash.c5ditj8vhg0k.us-west-1.rds.amazonaws.com"
-DB_PORT = "5432"
-DB_NAME = "jira"
-DB_USER = "redash"
-DB_PASSWORD = "N6ZrFz8KdR"
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Shell command to run the PostgreSQL function
 command = f"PGPASSWORD={DB_PASSWORD} psql -h {DB_HOST} -p {DB_PORT} -U {DB_USER} -d {DB_NAME} -c 'SELECT transform_issue_data(false, 1);'"
